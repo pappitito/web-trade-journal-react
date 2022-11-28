4ee89cb0fba8225d0dea0bff26ff820c7e3ce2de
Tito <107757681+pappitito@users.noreply.github.com>
2022-11-28 21:34:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
 
   return (
     <div className="App">
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/web-trade-journal-react'>
         <Routes>
-            <Route exact path='/' element={<Landing />} />
+            <Route exact path='/web-trade-journal-react' element={<Landing />} />
             <Route   path='login' element={<Login />} />
             <Route  path='register' element={<Register />} />
             <Route exact path='register/dashboard' element={<Dashboard />} />
